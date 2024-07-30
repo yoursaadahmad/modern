14443e0a04c35658433dde56212316f82d139a64
Saad Ahmad Raza <159026858+yoursaadahmad@users.noreply.github.com>
2024-07-30 11:26:30 +0500

    Update RecentProjects.tsx

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -43,7 +43,7 @@ const RecentProjects = () => {
                                 ))}
                             </div>
                             <div className='flex justify-center items-center'>
-                                <a><target href={link}><p className='flex lg:text-xl md:text-xs text-sm text-purple'>Live Site</p></target></a>
+                                <a href={link}><p className='flex lg:text-xl md:text-xs text-sm text-purple'>Live Site</p></a>
                                 <FaLocationArrow className="ms-3" color="#CBACF9" />
                             </div>
                         </div>
